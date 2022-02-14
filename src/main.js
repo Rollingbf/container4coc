0aff01ae2b417bf182b0aed4823891d7f3fc8225
XiaTian <1226818156@qq.com>
2022-02-14 16:24:51 +0800

    创建了基础表单模板（仍需考虑是否需要用表单标签作提交）

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/css/index.css";
-import router from './router'
+import router from "./router";
+import ElementUI from "element-ui";
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/creatorPage.vue
+++ src/views/creatorPage.vue
@@ -2,22 +2,57 @@
   <div class="h-screen">
     <Header />
     <div id="main" class="h-full pl-16 pr-8">
-      <div id="workBench" class="pt-20">
+      <div id="workBench" class="pt-20 w-2/3">
         <form
-          class="mb-3"
+          class="mb-3 ml-40 flex flex-col"
           v-for="(formItem, formIndex) in formList"
           :key="formItem.formId"
           :index="formIndex"
         >
-          <div
-            v-for="(inputItem, inputIndex) in formList[formIndex].inputList"
-            :key="inputItem.inputId"
-            :index="inputIndex"
-          >
-            <input class="rounded-lg bg-gray-200" v-model="inputItem.info" />
+          <a class="w-full">
+            <p class="w-1/5 self-center text-3xl">场景{{ formIndex + 1 }}</p>
+          </a>
+          <div class="flex">
+            <ul class="flex flex-col gap-3">
+              <li
+                class="flex"
+                v-for="(inputItem, inputIndex) in formList[formIndex].inputList"
+                :key="inputItem.inputId"
+                :index="inputIndex"
+              >
+                <a class="self-center">描述{{ inputIndex + 1 }}</a>
+                <span
+                  class="rounded-lg border-4 border-gray-200 h-36 w-96 pl-3 pb-2.5 flex relative"
+                >
+                  <textarea
+                    class="w-11/12 resize-none mt-3 mr-3 bg-transparent overflow-y-scroll focus:outline-none"
+                    v-model="inputItem.info"
+                  />
+                  <button
+                    @click="onDropInput(formIndex, inputIndex)"
+                    title="删除当前描述"
+                    class="rounded-lg w-5 h-5 absolute top-0 right-0 hover:text-red-600 text-xl"
+                  >
+                    ╳
+                  </button>
+                  <button
+                    title="清空当前描述"
+                    class="rounded-lg mb-2 mr-1 w-5 h-5 absolute bottom-0 right-0 hover:text-red-600 text-xl"
+                  >
+                    ←
+                  </button>
+                </span>
+              </li>
+            </ul>
+            <a
+              title="新增描述"
+              @click="onAddInput(formIndex)"
+              class="rounded-full w-9 py-1 px-3 cursor-pointer select-none ml-3 bg-blue-300 self-end"
+              >+</a
+            >
           </div>
-          <a @click="onAddInput" class="ml-3 bg-red-100">+</a>
         </form>
+        <a @click="onAddForm" class="ml-3 bg-red-100 self-end">+</a>
       </div>
     </div>
   </div>
@@ -45,15 +80,32 @@ export default {
     };
   },
   methods: {
-    onAddInput() {
-      this.formList[this.formIndex].inputIndex++;
-      this.formList[this.formIndex].inputList.push({
-        inputId: String.fromCharCode(
-          this.formList[this.formIndex].inputIndex + 97
-        ),
+    onAddInput(t) {
+      this.formList[t].inputIndex++;
+      this.formList[t].inputList.push({
+        inputId: String.fromCharCode(this.formList[t].inputIndex + 97),
         info: "",
       });
-      console.log(this.formList);
+    },
+    onAddForm() {
+      this.formIndex++;
+      this.formList.push({
+        formId: this.formIndex + 1,
+        inputIndex: 1,
+        inputList: [
+          {
+            inputId: "a",
+            info: "",
+          },
+        ],
+      });
+    },
+    onDropInput(tF, tI) {
+      if (this.formList[tF].inputList.length < 2) {
+        this.formList.splice(tF, 1);
+      } else {
+        this.formList[tF].inputList.splice(tI, 1);
+      }
     },
   },
   components: {
